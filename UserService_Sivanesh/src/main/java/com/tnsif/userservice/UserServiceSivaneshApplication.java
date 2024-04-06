7974ae1747b4a40e88306f567c54bc7351291222
sudhans03 <sudhanprogram@gmail.com>
2024-04-06 21:05:04 +0530

    It is my sprint1 project

---
+++ UserService_Sivanesh/src/main/java/com/tnsif/userservice/UserServiceSivaneshApplication.java
@@ -0,0 +1,13 @@
+package com.tnsif.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceSivaneshApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceSivaneshApplication.class, args);
+	}
+
+}
